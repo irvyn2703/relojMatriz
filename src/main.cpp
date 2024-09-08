17f6e16f47f38d07a600526aafa10eea9a65f9d0
irvyn2703 <tosolatoso17@gmail.com>
2024-09-07 18:21:48 -0600

    agregando animaciones

--- src/main.cpp
+++ src/main.cpp
@@ -31,16 +31,18 @@ int diaSemana = 0;
 int rojo = 0;
 int verde = 0;
 int azul = 255;
-int brillo = 5;
+int brillo = 10;
 int tempBrillo = 5;
 static unsigned long twoPoints = 0;
 static unsigned long animacion = 0;
 bool twoP = true;
-int numAnim = 1;
-int anim = 4;
+int numAnim = 3;
+int anim = 6;
 int intervaloAnimaciones = 20000;
+int hh = 0;
+int mm = 0;
 
-int figuras[18][SCREEN_ROWS][SCREEN_ROWS] = {
+int figuras[30][SCREEN_ROWS][SCREEN_ROWS] = {
   // whatsapp
   { 
     {0  , 0  , 255, 255, 255, 255, 0  , 0  },
@@ -227,6 +229,127 @@ int figuras[18][SCREEN_ROWS][SCREEN_ROWS] = {
     {0  , 90 , 90 , 90 , 50 , 90 , 50 , 0  },
     {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
   },
+  //amongUs
+  { 
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 255, 255, 255, 0  , 0  , 0  , 0  },
+    {255, 255, 0  , 0  , 0  , 0  , 0  , 0  },
+    {255, 255, 255, 255, 0  , 0  , 0  , 0  },
+    {0  , 255, 255, 255, 0  , 0  , 0  , 0  },
+    {0  , 255, 0  , 255, 0  , 0  , 0  , 0  },
+  },
+  { 
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 255, 255, 255, 0  , 0  , 0  , 0  },
+    {255, 255, 0  , 0  , 0  , 0  , 0  , 0  },
+    {255, 255, 255, 255, 0  , 0  , 0  , 0  },
+    {0  , 255, 255, 255, 0  , 0  , 0  , 0  },
+    {0  , 255, 0  , 255, 0  , 0  , 0  , 0  },
+  },
+  { 
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 255, 255, 255, 0  , 0  , 0  , 0  },
+    {255, 255, 0  , 0  , 0  , 0  , 0  , 0  },
+    {255, 255, 255, 255, 0  , 0  , 0  , 0  },
+    {0  , 255, 255, 255, 0  , 0  , 0  , 0  },
+    {0  , 255, 0  , 255, 0  , 0  , 0  , 0  },
+  },
+  { 
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 255, 255, 255, 0  , 0  , 0  , 0  },
+    {255, 255, 0  , 0  , 0  , 0  , 0  , 0  },
+    {255, 255, 255, 255, 0  , 0  , 0  , 0  },
+    {0  , 255, 255, 255, 0  , 0  , 0  , 0  },
+    {0  , 0  , 255, 0  , 0  , 0  , 0  , 0  },
+  },
+  { 
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 255, 255, 255, 0  , 0  , 0  , 0  },
+    {255, 255, 0  , 0  , 0  , 0  , 0  , 0  },
+    {255, 255, 255, 255, 0  , 0  , 0  , 0  },
+    {0  , 255, 255, 255, 0  , 0  , 0  , 0  },
+    {0  , 0  , 255, 0  , 0  , 0  , 0  , 0  },
+  },
+  { 
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 255, 255, 255, 0  , 0  , 0  , 0  },
+    {255, 255, 0  , 0  , 0  , 0  , 0  , 0  },
+    {255, 255, 255, 255, 0  , 0  , 0  , 0  },
+    {0  , 255, 255, 255, 0  , 0  , 0  , 0  },
+    {0  , 0  , 255, 0  , 0  , 0  , 0  , 0  },
+  },
+  { 
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 255, 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 255, 0  , 0  , 0  , 0  , 0  },
+    {255, 255, 255, 255, 0  , 0  , 0  , 0  },
+    {0  , 255, 255, 255, 0  , 0  , 0  , 0  },
+    {0  , 255, 0  , 255, 0  , 0  , 0  , 0  },
+  },
+  { 
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 255, 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 255, 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+  },
+  { 
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 255, 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 255, 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+  },
+  { 
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 137, 137, 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+  },
+  { 
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 188, 188, 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+  },
+  { 
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 207, 207  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+    {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  },
+  },
 };
 
 int numeros[11][NUM_ROWS][NUM_COLS] = {
@@ -401,6 +524,24 @@ void showFigura(int number, int startX, int startY) {
   }
 }
 
+void showAmongUs(int number, int startX, int startY, int R, int G, int B) {
+  for (int x = 0; x < SCREEN_ROWS; x++) {
+    for (int y = 0; y < SCREEN_ROWS; y++) {
+      if (figuras[number*3][x][y] != 0 || figuras[number*3+1][x][y] != 0 || figuras[number*3+2][x][y] != 0) {
+        int ledIndex = 0;
+        if ((startY + y) % 2 == 0) {
+          ledIndex = (x + startX) + (startY + y) * SCREEN_ROWS;
+        } else {
+          ledIndex = (SCREEN_ROWS - x - 1 - startX) + (startY + y) * SCREEN_ROWS;
+        }
+        if (x + startX < 8 && x + startX >= 0) {
+          pantalla.setPixelColor(ledIndex, pantalla.Color(figuras[number*3][x][y]/R, figuras[number*3+1][x][y]/G, figuras[number*3+2][x][y]/B));
+        }
+      }
+    }
+  }
+}
+
 // Función para obtener la hora de la API
 void obtenerHora() {
   HTTPClient http;
@@ -424,11 +565,11 @@ void obtenerHora() {
 // Función para mostrar la hora en la pantalla LED
 void mostrarHora(int y, int x) {
   if (hora_actual.length() == 5) {
-    int hh = hora_actual.substring(0, 2).toInt();
+    hh = hora_actual.substring(0, 2).toInt();
     if (hh > 12) {
       hh = hh - 12;
     }
-    int mm = hora_actual.substring(3, 5).toInt();
+    mm = hora_actual.substring(3, 5).toInt();
 
     if (millis() - twoPoints > 1000) {
       twoP = !twoP;
@@ -514,6 +655,13 @@ void setupSinricPro() {
   SinricPro.begin(API_KEY, APP_SECRET);
 }
 
+void reiniciarAnimacion(int &step){
+  animacion = millis();
+  step = 0;
+  anim = random(4, 4 + numAnim);
+  intervaloAnimaciones = random(10000, 600000);
+}
+
 void animAbeja(int &step, unsigned long &lastStepTime) {
     step++;
     if (step <= 10) {
@@ -544,13 +692,130 @@ void animAbeja(int &step, unsigned long &lastStepTime) {
                 mostrarHora(1, 20 - temp*2);
                 pantalla.show();
             } else {
-                animacion = millis();
-                step = 0;
-                anim = random(4, 4 + numAnim);
-                intervaloAnimaciones = random(20000, 900000);
+              reiniciarAnimacion(step);
             }
         }
     }
+    lastStepTime = millis();
+}
+
+void animAmongUs(int &step, unsigned long &lastStepTime) {
+    step++;
+    if (step <= 10) {
+        pantalla.clear();
+        mostrarHora(1, 0 + step*2);
+        pantalla.show();
+    } else {
+        if (step <= 25) {
+            int temp = step - 10;
+            pantalla.clear();
+            if (temp % 2 == 0){
+              showAmongUs(6,0,0 + (temp), 1, 255, 255);
+              showFigura(9,0,0 + (temp));
+            }else{
+              showAmongUs(7,0,0 + (temp), 1, 255, 255);
+              showFigura(9,0,0 + (temp));
+            }
+            
+            pantalla.show();
+        } else {
+          if (step <= 35)
+          {
+            int temp = step - 30;
+            pantalla.clear();
+            
+            showAmongUs(6,0,15, 1, 255, 255);
+            showFigura(9,0,15);
+
+            if (temp % 2 == 0){
+              showAmongUs(6,0,0 + (temp*2), hh/3 + 1, mm/15 + 1, (hh/3 + mm/15)/2 + 1);
+              showFigura(9,0,0 + (temp*2));
+            }else{
+              showAmongUs(7,0,0 + (temp*2), hh/3 + 1, mm/15 + 1, (hh/3 + mm/15)/2 + 1);
+              showFigura(9,0,0 + (temp*2));
+            }
+
+            pantalla.show();
+          } else {
+            if (step <= 39)
+            {
+              if (step == 36)
+              {
+                pantalla.clear();
+              }
+              
+
+              int temp = step - 35;
+              
+              int x = 5;  
+              int y = 15 + temp;
+              int ledIndex = 0;
+
+              if (y % 2 == 0) {
+                ledIndex = x + (y) * SCREEN_ROWS;
+              } else {
+                ledIndex = (SCREEN_ROWS - x - 1) + (y) * SCREEN_ROWS;
+              }
+
+              if (x < 8 && x >= 0) {
+                pantalla.setPixelColor(ledIndex, pantalla.Color(255, 255, 255));
+              }
+
+              pantalla.show();
+            } else{
+              if(step <= 70){
+                pantalla.clear();
+                
+
+                int temp = step - 39;
+                
+                showFigura(8,0,15);
+
+                showAmongUs(6,0,18 + temp, hh/3 + 1, mm/15 + 1, (hh/3 + mm/15)/2 + 1);
+                showFigura(9,0,18 + temp);
+
+                pantalla.show();
+
+              }else{
+                if (step <= 80) {
+                    int temp = step - 70;
+                    pantalla.clear();
+                    mostrarHora(1, 20 - temp*2);
+                    pantalla.show();
+                } else {
+                  reiniciarAnimacion(step);
+                }
+              }
+            }
+          }
+        }
+    }
+    lastStepTime = millis();
+}
+
+void animAmongUsRun(int &step, unsigned long &lastStepTime) {
+    step++;
+    if (step <= 70) {
+        pantalla.clear();
+
+        mostrarHora(1, 0);
+
+        if (step % 2 == 0){
+          showAmongUs(6,0,0 + (step), 1, 255, 255);
+          showFigura(9,0,0 + (step));
+          showAmongUs(6,0,-70 + step*2.5, hh/3 + 1, mm/15 + 1, (hh/3 + mm/15)/2 + 1);
+          showFigura(9,0,-70 + step*2.5);
+        }else{
+          showAmongUs(7,0,0 + (step), 1, 255, 255);
+          showFigura(9,0,0 + (step));
+          showAmongUs(7,0,-70 + step*2.5, hh/3 + 1, mm/15 + 1, (hh/3 + mm/15)/2 + 1);
+          showFigura(9,0,-70 + step*2.5);
+        }
+        pantalla.show();
+    } else{
+      reiniciarAnimacion(step);
+    }
+          
     lastStepTime = millis();
 }
 
@@ -565,7 +830,12 @@ void animIntermedio(int numAnim){
     case 4:
       animAbeja(step, lastStepTime);
       break;
-    
+    case 5:
+      animAmongUs(step, lastStepTime);
+      break;
+    case 6:
+      animAmongUsRun(step, lastStepTime);
+      break;
     
     default:
       step++;
